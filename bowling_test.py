67d55e9a1d8dd31a92f9bc24a12b413ac79da1fd
JamesLau2001 <james_lau2001@outlook.com>
2025-06-11 16:44:12 +0100

    added more tests for existing solutions

--- bowling_test.py
+++ bowling_test.py
@@ -10,6 +10,11 @@ class TestBowlingScore(unittest.TestCase):
     
     def test_scores_45(self):
         self.assertEqual(bowling_score("45"), 9)
+        self.assertEqual(bowling_score("33"), 6)
+        self.assertEqual(bowling_score("53"), 8)
 
     def test_score_spare(self):
         self.assertEqual(bowling_score("6/ 42"), 20)
+        self.assertEqual(bowling_score("3/ 71"), 25)
+        self.assertEqual(bowling_score("8/ 52"), 22)
+        self.assertEqual(bowling_score("8/ 2/ 33"), 31)
