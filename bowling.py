8cbf4fe2229a9944ba10008412fae359ea64a6c2
JamesLau2001 <james_lau2001@outlook.com>
2025-06-11 14:04:51 +0100

    tested for 1 round of 2 bowls

--- bowling.py
+++ bowling.py
@@ -1,2 +1,9 @@
 def bowling_score(score):
-    return 0
+    if score == "X":
+        return 10
+    elif len(score) == 2:
+        split_score = list(score)
+        total_score = int(split_score[0]) + int(split_score[1])
+        return total_score 
+    else:
+        return 0
--- bowling_test.py
+++ bowling_test.py
@@ -5,5 +5,8 @@ class TestBowlingScore(unittest.TestCase):
     def test_scores_0(self):
         self.assertEqual(bowling_score(""), 0)
     
-
+    def test_scores_x(self):
+        self.assertEqual(bowling_score("X"), 10)
     
+    def test_scores_45(self):
+        self.assertEqual(bowling_score("45"), 9)
