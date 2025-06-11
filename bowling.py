020e1194a7477d4cd1b575da35f246b825c10648
JamesLau2001 <james_lau2001@outlook.com>
2025-06-11 17:14:59 +0100

    added test for non-consecutive strikes

--- bowling.py
+++ bowling.py
@@ -7,19 +7,24 @@ def bowling_score(score):
     while current_frame_idx < len(frames_list):
         current_frame = frames_list[current_frame_idx]
 
-        if current_frame == "X":
+        # handling strikes
+        if current_frame == "X" and current_frame_idx == len(frames_list) - 1:
             total_score += 10
 
-        elif len(current_frame) == 2 and '/' not in current_frame:           
-            total_score += int(current_frame[0]) + int(current_frame[1])
+        elif current_frame == "X":
+            next_frame = frames_list[current_frame_idx + 1]        
+            total_score += 10 + int(next_frame[0]) + int(next_frame[1])
 
-        elif len(current_frame) == 2 and '/' in current_frame:
+        # handling spares
+        if len(current_frame) == 2 and '/' in current_frame:
             next_frame = frames_list[current_frame_idx + 1]
             first_score_of_next_frame = int(next_frame[0])           
             total_score += 10 + first_score_of_next_frame
+
+        # handling non-strikes and non-spares
+        if len(current_frame) == 2 and '/' not in current_frame:           
+            total_score += int(current_frame[0]) + int(current_frame[1])
         
         current_frame_idx += 1
     
     return total_score
-
-
--- bowling_test.py
+++ bowling_test.py
@@ -5,16 +5,22 @@ class TestBowlingScore(unittest.TestCase):
     def test_scores_0(self):
         self.assertEqual(bowling_score(""), 0)
     
-    def test_scores_x(self):
-        self.assertEqual(bowling_score("X"), 10)
-    
-    def test_scores_45(self):
+    def test_scores_one_frame(self):
         self.assertEqual(bowling_score("45"), 9)
         self.assertEqual(bowling_score("33"), 6)
         self.assertEqual(bowling_score("53"), 8)
 
-    def test_score_spare(self):
+    def test_score_one_spare(self):
         self.assertEqual(bowling_score("6/ 42"), 20)
         self.assertEqual(bowling_score("3/ 71"), 25)
         self.assertEqual(bowling_score("8/ 52"), 22)
+
+    def test_score_multiple_spares(self):    
         self.assertEqual(bowling_score("8/ 2/ 33"), 31)
+
+    def test_score_non_consecutive_strikes(self):
+        self.assertEqual(bowling_score("X"), 10)
+        self.assertEqual(bowling_score("X 43 2/ 33"), 43)
+        self.assertEqual(bowling_score("X 43 X"), 34)
+        
+    
